ec461f0d9f2fd95851c48df22bc031e54cabb9ae
RanjanBa <ranjanbaro2015@gmail.com>
2020-12-27 20:13:17 +0530

    new rounds

---
+++ CodeForces/CP/Round691/a.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define phb push_back
+#define ppb pop_back
+#define arr array
+#define FOR(i, x) for (int i = 0; i < x; i++)
+#define FORS(i, s, x) for (int i = s; i < x; i++)
+#define FORR(i, x) for (int i = x - 1; i >= 0; i--)
+#define FORV(v, li) for (auto v : li)
+#define SZ(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define un_set unordered_set
+#define un_map unordered_map
+#define mem(li, v) memset(li, v, sizeof(li))
+#define INF 1e9
+#define INFL 1e18
+
+using namespace std;
+
+template <typename T>
+inline void debug(vector<T> li)
+{
+    FORV(v, li)
+    {
+        cout << v << " ";
+    }
+
+    cout << endl;
+}
+
+template <typename T>
+inline void debug(vector<vector<T>> li)
+{
+    FORV(v1, li)
+    {
+        FORV(v2, v1)
+        {
+            cout << v2 << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+const int mxN = 2e5;
+const int di[4] = {1, 0, -1, 0}, dj[4] = {0, 1, 0, -1};
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string a, b;
+    cin >> a >> b;
+
+    int red = 0, eq = 0, blue = 0;
+
+    FOR(i, n)
+    {
+        if (a[i] > b[i])
+        {
+            red++;
+        }
+        else if (a[i] == b[i])
+        {
+            eq++;
+        }
+        else
+        {
+            blue++;
+        }
+    }
+
+    if (red > blue)
+    {
+        cout << "RED\n";
+    }
+    else if (red < blue)
+    {
+        cout << "BLUE\n";
+    }
+    else
+    {
+        cout << "EQUAL\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+
+    cin >> T;
+
+    for (int t = 1; t <= T; t++)
+    {
+        solve();
+    }
+
+    return 0;
+}
