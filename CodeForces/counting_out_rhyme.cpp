9e5cc26c985022e2ae3773fbafad55cc3a660a95
RanjanBa <ranjanbaro2015@gmail.com>
2021-01-20 18:45:25 +0530

    new cp

---
+++ CodeForces/counting_out_rhyme.cpp
@@ -0,0 +1,90 @@
+//TODO: Not completed
+
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define phb push_back
+#define ppb pop_back
+#define arr array
+#define rep(i, s, x, inc) for (int i = s; i < x; i += inc)
+#define repr(i, e, x, dec) for (int i = x - 1; i >= e; i -= dec)
+#define repv(v, li) for (auto v : li)
+#define sz(li) (int)li.size()
+#define all(v) v.begin(), v.end()
+#define un_set unordered_set
+#define un_map unordered_map
+#define mem(li, v) memset(li, v, sizeof(li))
+#define INF 1e9
+#define INFL 1e18
+
+using namespace std;
+
+#define whatis(x) cerr << #x << " : " << x << "\n";
+
+template <typename T>
+inline void debug(vector<T> li)
+{
+    repv(v, li)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+
+template <typename T>
+inline void debug(vector<vector<T>> li)
+{
+    repv(v1, li)
+    {
+        repv(v2, v1)
+        {
+            cout << v2 << " ";
+        }
+
+        cout << "\n";
+    }
+}
+
+// ------ main function ------
+
+const int mxN = 2e5;
+const int di[4] = {1, 0, -1, 0}, dj[4] = {0, 1, 0, -1};
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> vec(k);
+
+    rep(i, 0, k, 1)
+    {
+        cin >> vec[i];
+    }
+
+    vector<int> child(n), res;
+    rep(i, 0, n, 1)
+    {
+        child[i] = i + 1;
+    }
+
+    int cur = 0;
+    rep(i, 0, k, 1)
+    {
+    }
+
+    repv(v, res)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+    return 0;
+}
